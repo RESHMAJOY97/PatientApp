385580189dca6929077c487c760fd5140856fa08
RESHMAJOY97 <reshmajoy97@gmail.com>
2020-12-12 18:51:23 +0530

    values readed

--- app/src/main/java/com/example/patientapp/MainActivity.kt
+++ app/src/main/java/com/example/patientapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.patientapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun funcionsubmit(view: View) {
+        var getname=findViewById<EditText>(R.id.name)
+        var getdoctor=findViewById<EditText>(R.id.doctor)
+        var getsymptoms=findViewById<EditText>(R.id.symptoms)
+        Toast.makeText(this,getname.text.toString(),Toast.LENGTH_LONG).show()
+        Toast.makeText(this,getdoctor.text.toString(),Toast.LENGTH_LONG).show()
+        Toast.makeText(this,getsymptoms.text.toString(),Toast.LENGTH_LONG).show()
+        }
 }
